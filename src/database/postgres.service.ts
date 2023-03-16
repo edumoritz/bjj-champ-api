a66839411e028827cd66fbbb96e962f243b4ebd6
edumoritz <eduardo.moritz@hotmail.com>
2023-03-16 10:07:43 -0300

    feat - implementation of swagger

--- src/database/postgres.service.ts
+++ src/database/postgres.service.ts
@@ -4,15 +4,16 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 @Injectable()
 export class PostgresService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      synchronize: true,
-    };
+    const configConnection: TypeOrmModuleOptions = {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [],
+        synchronize: true,
+    }
+    return configConnection;
   }
 }
